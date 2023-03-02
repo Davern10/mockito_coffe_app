62803d3915a991a4f4bdc5f0c43c54e03dce2ca0
David Hernandez <davern1999@gmail.com>
2023-03-02 14:40:09 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/mockito_coffe_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mockito_coffe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'app/routes/app_pages.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light));
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+}
